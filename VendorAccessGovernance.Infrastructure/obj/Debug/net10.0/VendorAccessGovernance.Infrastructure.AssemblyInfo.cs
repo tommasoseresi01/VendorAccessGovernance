542072156e8e38aec9e7e535b6f709ebde3538bf
Tommaso Seresi <tommaso.seresi@studenti.unicam.it>
2026-06-15 14:14:27 +0200

    Commit for right configuration

--- VendorAccessGovernance.Infrastructure/obj/Debug/net10.0/VendorAccessGovernance.Infrastructure.AssemblyInfo.cs
+++ VendorAccessGovernance.Infrastructure/obj/Debug/net10.0/VendorAccessGovernance.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VendorAccessGovernance.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec01965fe15d2b8798d6ae74577b55462897754")]
 [assembly: System.Reflection.AssemblyProductAttribute("VendorAccessGovernance.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VendorAccessGovernance.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

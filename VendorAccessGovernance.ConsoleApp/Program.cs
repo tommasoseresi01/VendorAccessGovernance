8ec01965fe15d2b8798d6ae74577b55462897754
Tommaso Seresi <tommaso.seresi@studenti.unicam.it>
2026-06-15 14:12:09 +0200

    first commit

---
+++ VendorAccessGovernance.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using VendorAccessGovernance.Core;
+
+Console.WriteLine("Hello, World!");
+
+HelloNewProgram NewProgram = new HelloNewProgram("Vendor Access Governance");
+
+NewProgram.HelloWorldVendor();
---
+++ VendorAccessGovernance.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendorAccessGovernance.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
